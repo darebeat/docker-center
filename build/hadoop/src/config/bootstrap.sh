5f5b0a9d7f8b86f16a5a15ed9f05bfeaca871697
Darebeat <fengwei2010@126.com>
2019-08-31 22:18:40 +0800

    hadoop init version

---
+++ build/hadoop/src/config/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+
+$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+
+# service sshd start
+$HADOOP_PREFIX/sbin/start-dfs.sh
+
+echo -e "\n"
+
+$HADOOP_PREFIX/sbin/start-yarn.sh
+# $HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
