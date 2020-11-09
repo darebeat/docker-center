336b76f21c725701d8c7d21d5a44b0a73b03e3b2
Darebeat <fengwei2010@126.com>
2020-11-09 17:39:27 +0800

    add: mblog backup mark

---
+++ deploy/mblog/backup.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# 1. 备份数据库
+docker exec -it mysql57 bash
+mysqldump -u root -p --databases mblog > /bak.sql
+exit
+
+# 2. 备份存储
+docker cp mysql57:/bak.sql ./data
+tar zcvf mblog.tar.gz ./data
+
+
+# 3. 迁移到新数据库或部署环境
+tar zxvf mblog.tar.gz
+
+docker cp ./data/bak.sql mysql57:/bak.sql
+docker exec -it mysql57 bash
+mysql -uroot -p < /bak.sql
+exit
+
+# 启动应用程序
+docker-compose up -d
