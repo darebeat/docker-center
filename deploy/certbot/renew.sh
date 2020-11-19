ea80ceb65b3ce2b119069ffffdbcd2e98b47c9a1
Darebeat <fengwei2010@126.com>
2020-11-19 21:43:19 +0800

    adj: certbot auto commit

--- deploy/certbot/renew.sh
+++ deploy/certbot/renew.sh
@@ -2,26 +2,28 @@
 
 declare -r CURR_DIR=$(cd `dirname $0`;pwd)
 
-echo "-----START " `date +"%Y-%m-%d %H:%M:%S"` "-----"  >> renew.log
+echo "-----START " `date +"%Y-%m-%d %H:%M:%S"` "-----"  >> ${CURR_DIR}/renew.log
 
 # # dnspod
 # docker run --rm -it \
-# -v `pwd`/dnspod.conf:/opt/certbot/dnspod.conf \  # dnspod配置文件
-# -v `pwd`/etc:/etc/letsencrypt \
-# -v `pwd`/lib:/var/lib/letsencrypt \
-# -v `pwd`/log:/var/log/letsencrypt \
-# darebeat/certbot renew  >> renew.log 2>&1
+# -v ${CURR_DIR}/dnspod.conf:/opt/certbot/dnspod.conf \  # dnspod配置文件
+# -v ${CURR_DIR}/etc:/etc/letsencrypt \
+# -v ${CURR_DIR}/lib:/var/lib/letsencrypt \
+# -v ${CURR_DIR}/log:/var/log/letsencrypt \
+# darebeat/certbot renew  >> ${CURR_DIR}/renew.log 2>&1
 
 # hwy/aly/txy/godaddy
 docker run --rm -it \
--v `pwd`/etc:/etc/letsencrypt \
--v `pwd`/lib:/var/lib/letsencrypt \
--v `pwd`/log:/var/log/letsencrypt \
--v `pwd`/domains:/opt/certbot/certbot-dns-cnyun/domains \  # 根据需要进行扩展
---env-file `pwd`/.env \  # api key/token config
+-v ${CURR_DIR}/etc:/etc/letsencrypt \
+-v ${CURR_DIR}/lib:/var/lib/letsencrypt \
+-v ${CURR_DIR}/log:/var/log/letsencrypt \
+-v ${CURR_DIR}/domains:/opt/certbot/certbot-dns-cnyun/domains \  # 根据需要进行扩展
+--env-file ${CURR_DIR}/.env \  # api key/token config
 darebeat/certbot renew
   --manual --preferred-challenges dns \
   --manual-auth-hook "/opt/certbot/certbot-dns-cnyun/dns-flush.sh hwy add" \
-  --manual-cleanup-hook "/opt/certbot/certbot-dns-cnyun/dns-flush.sh hwy clean" >> renew.log 2>&1
+  --manual-cleanup-hook "/opt/certbot/certbot-dns-cnyun/dns-flush.sh hwy clean" >> ${CURR_DIR}/renew.log 2>&1
 
-echo "-----END" `date +"%Y-%m-%d %H:%M:%S"`"-----"  >> renew.log
+scp -r ${CURR_DIR}/etc/darebeat.cn txy:/root/projects/docker-center/deploy/nginx/conf/certs >> ${CURR_DIR}/renew.log
+
+echo "-----END" `date +"%Y-%m-%d %H:%M:%S"`"-----"  >> ${CURR_DIR}/renew.log
