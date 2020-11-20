df6f190597a1184002a7b1531621d96c6c0c3363
Darebeat <fengwei2010@126.com>
2020-11-20 11:25:16 +0800

    adj: certbot renew.sh modify

--- deploy/certbot/renew.sh
+++ deploy/certbot/renew.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # USAGE:
-## bash +x renew.sh txy /etc/nginx/ssl
+## sh +x renew.sh txy /etc/nginx/ssl
 
 declare -r CURR_DIR=$(cd `dirname $0`;pwd)
 
@@ -9,11 +9,12 @@ COPY_PATH=${CURR_DIR}/etc
 TARG_PATH=${2:-${CURR_DIR}/../nginx/conf/certs}
 LOG_PATH=${CURR_DIR}/log
 
-echo "--- START: " `date +"%Y-%m-%d %H:%M:%S"` "---" >> ${LOG_PATH}/renew.log
+echo "--- START: `date +"%Y-%m-%d %H:%M:%S"` ---
+COPY_PATH: ${COPY_PATH}
+TARG_PATH: ${TARG_PATH}
+LOG_PATH : ${LOG_PATH} " >> ${LOG_PATH}/renew.log
 
-echo "COPY_PATH: ${COPY_PATH}"
-echo "TARG_PATH: ${TARG_PATH}"
-echo "LOG_PATH : ${LOG_PATH}"
-#scp -r ${COPY_PATH} ${1:-txy}:${TARG_PATH} >> ${LOG_PATH}/renew.log
+scp -r ${COPY_PATH} ${1:-txy}:${TARG_PATH} >> ${LOG_PATH}/renew.log
 
-echo "---   END: " `date +"%Y-%m-%d %H:%M:%S"`"---" >> ${LOG_PATH}/renew.log
+echo "---   END: `date +"%Y-%m-%d %H:%M:%S"` ---
+" >> ${LOG_PATH}/renew.log
