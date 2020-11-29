e05fa6a7db1145c9ae619c6441c7861a59740f62
Darebeat <fengwei2010@126.com>
2020-11-29 23:59:06 +0800

    adj: mblog docker-compose auto commit

--- deploy/mblog/backup.sh
+++ deploy/mblog/backup.sh
@@ -6,14 +6,14 @@ mysqldump -u root -p --databases mblog > /bak.sql
 exit
 
 # 2. 备份存储
-docker cp mysql57:/bak.sql ./data
-tar zcvf mblog.tar.gz ./data
+docker cp mysql57:/bak.sql ./storage
+tar zcvf mblog.tar.gz ./storage
 
 
 # 3. 迁移到新数据库或部署环境
 tar zxvf mblog.tar.gz
 
-docker cp ./data/bak.sql mysql57:/bak.sql
+docker cp ./storage/bak.sql mysql57:/bak.sql
 docker exec -it mysql57 bash
 mysql -uroot -p < /bak.sql
 exit
